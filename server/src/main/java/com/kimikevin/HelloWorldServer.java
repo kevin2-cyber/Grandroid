0073b5b9def65cee8afee134c61ddbb6fa9bb1a7
kevin2-cyber <keduful@st.ug.edu.gh>
2024-10-12 13:42:56 +0000

    hello world project

---
+++ server/src/main/java/com/kimikevin/HelloWorldServer.java
@@ -0,0 +1,13 @@
+package com.kimikevin;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
+
+    private Server server;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
